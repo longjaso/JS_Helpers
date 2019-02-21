f4f247582859b269fa89ba781d66f11abc60d7fb
longjaso <longjaso@gmail.com>
2019-02-21 10:50:54 -0800

    Object Clone Creation
    This script will make a true clone of an object that can be manipulated without affecting the original. This is done by recursively operating based on the type of property. Tests are needed to verify the functionality of nesting.

---
+++ objectClone.js
@@ -0,0 +1,28 @@
+function deepClone(obj) {
+	var clone = {};
+	
+	function cloneArray(arr){
+		let clonedArr = [];
+		arr.forEach(index => {
+			if (typeof(index) !== 'object') {
+				clonedArr.push(index);
+			} else if (Array.isArray(index)){
+				clonedArr.push(cloneArray(index));
+			} else {
+				clonedArr.push(deepClone(index));
+			}
+		})
+		return clonedArr;
+	}
+	
+	for (let prop in obj){
+		if (typeof(obj[prop]) !== 'object') {
+			clone[prop] = obj[prop];
+		} else if (Array.isArray(obj[prop])) {
+			clone[prop] = cloneArray(obj[prop]);
+		} else {
+			clone[prop] = deepClone(obj[prop]);
+		}
+	}
+	return clone;
+}
